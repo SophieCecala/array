9df87550b436a154b41e2dd85e1a8c714d047063
Natalya Myunster <natalyamyunster@yahoo.com>
2021-01-27 20:04:40 +0100

    tasks 1, 2, 3, 4

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //1
+/*
+Create an array with elements 'a', 'b' and 'c'. Display it on the screen using the alert function, and also output it to the console using console.log.
+ */
+// const arr = ["a", "b", "c"];
+// alert(arr);
+// console.log(arr);
+
+
+
+//2
+/*
+Create an array with elements 'a', 'b', 'c'. Display each of these elements.
+ */
+
+// const arr = ["a", "b", "c"];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+
+//3
+/*
+Create an array with elements An array with elements 'a', 'b', 'c', 'd'. Use it to display the string 'a + b + c + d'.
+ */
+
+// const arr = ["a", "b", "c", "d"];
+// console.log(`${arr[0]} + ${arr[1]} + ${arr[2]} + ${arr[3]}`);
+
+//4
+/*
+Create an array with elements 1, 2, and 3. Print the sum of these elements.
+ */
+
+const num = [1, 2, 3];
+const sum = num[0] + num[1] + num[2];
+console.log(sum);
+
+
+//5
+/*
+Create an array with elements 2, 5, 3, 9. Multiply the first element of the array by the second and the third element by the fourth. Add the results, assign the result variable. Display the value of this variable.
+
+ */
